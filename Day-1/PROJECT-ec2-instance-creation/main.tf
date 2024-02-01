36a050ec7edfb4e92d22e18e7af9942fdf2771f9
napamaheshsumanth <158264417+napamaheshsumanth@users.noreply.github.com>
2024-02-01 15:16:22 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-west-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0ce2cb35386fc22e9"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0753b2b54716054f5"
+    key_name = "west key pair"
 }
